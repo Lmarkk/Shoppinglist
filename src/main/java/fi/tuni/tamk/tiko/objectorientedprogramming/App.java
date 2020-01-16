752a58ab81320d175a43dd11ad27d8da71e8462c
Sorellum <48249159+Sorellum@users.noreply.github.com>
2020-01-16 20:07:19 +0200

    Add files via upload

---
+++ src/main/java/fi/tuni/tamk/tiko/objectorientedprogramming/App.java
@@ -0,0 +1,18 @@
+package fi.tuni.tamk.tiko.objectorientedprogramming;
+
+/**
+ * Class from which the shoppinglist application launches. 
+ * 
+ * @author Lassi Markkinen
+ * @version 2019.1111
+ */
+public class App {
+    /**
+     * Main method which is used to launch the shoppinglist application.
+     * @param args Not used.
+     */
+    public static void main(String[] args) {
+        System.out.println("Author: Lassi Markkinen");
+        Gui.main(args);
+    }
+}
